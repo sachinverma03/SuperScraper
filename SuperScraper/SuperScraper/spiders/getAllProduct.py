76db02801d045728d35c439b548e1c19ab983858
Sachin Verma <sachin9a.m@gmail.com>
2019-08-13 01:14:15 +0530

    removed comments

--- SuperScraper/SuperScraper/spiders/getAllProduct.py
+++ SuperScraper/SuperScraper/spiders/getAllProduct.py
@@ -12,17 +12,16 @@ class spiderMan(scrapy.Spider):
 
 
 	def parse(self, response):
-		yield scrapy.FormRequest.from_response(response, formdata={'username': '5achin', 'password': 'vsachi-.'}, 
-			formxpath = '//*[@id="rc-pagecontent"]/div/div/div/div[1]/form',
-                    callback=self.after_post)
+		yield scrapy.FormRequest.from_response(response, formdata={'username': '', 'password': ''}, 
+			formxpath = '//*[@id="rc-pagecontent"]/div/div/div/div[1]/form', callback=self.after_login) #xpath generated from page
 		
 
-	def after_post(self, response):
-		 print(response.css('a::text').getall())
-		# yield {'comics':response.css('div.vm-product-descr-container-1').css('a::Text').getall()}	#name from product decription div
+	def after_login(self, response):
+		 # print(response.css('a::text').getall())
+		yield {'comics':response.css('div.vm-product-descr-container-1').css('a::Text').getall()}	#name from product decription div
 
-		# links=response.css('a::attr(href)').getall()
-		# for link in links:
-		# 	link = response.urljoin(link)
-		# 	if(not link.endswith('-detail')): #for individual products
-		# 		yield scrapy.Request(link, callback=self.parse)
+		links=response.css('a::attr(href)').getall()
+		for link in links:
+			link = response.urljoin(link)
+			if(not link.endswith('-detail')): #for individual products
+				yield scrapy.Request(link, callback=self.parse)
